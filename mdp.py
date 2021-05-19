54fe5c446a2f0c8dd2ffc5e039e3a51941009ec5
beboy01 <74278314+beboy01@users.noreply.github.com>
2021-05-19 22:58:25 +0100

    Update mdp.py

--- mdp.py
+++ mdp.py
@@ -1,5 +1,18 @@
-def nbre():
-    mdp= input("entre le mot de pas ")
-    mdp=int(mdp)
-    if mdp == 0:
-        print("mot de pas trop cou ".upercase())
+
+def mot_de_passe(mdp):
+     
+     affiche=" "
+     max= 8
+     if len(mdp)==0:
+          affiche= "mot de passe trop cour "
+          affiche = affiche.upper()
+          print(affiche)
+     elif len(mdp) >0 and len(mdp)<= max:
+          affiche="mot de passe inferieur à 8"
+          affiche= affiche.capitalize()
+          print(affiche)
+     elif max <= len(mdp):
+          affiche= "mot de pas "
+          print(affiche)
+mdp = input("donne moit un mode passe : ")
+mot_de_passe(mdp)
