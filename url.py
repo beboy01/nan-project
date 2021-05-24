0e43e2b3e3a4e34b47b8a4815312ec137572aa87
beboy01 <74278314+beboy01@users.noreply.github.com>
2021-05-24 09:55:19 +0100

    Add files via upload

---
+++ url.py
@@ -0,0 +1,12 @@
+############# exo ###############
+
+
+# Ne modifiez pas les variables ci-dessous
+protocole = "https://"
+nom_du_site = "docstring"
+extension = "fr"
+page = "glossaire"
+
+# Modifiez le code Ã  partir d'ici
+URL = f"{protocole+nom_du_site+'.'+extension+'/'+page}"
+print(URL)
